54c9b113492ec5e68f06a01470f7ff8a7952fd7e
SemahaDundar <semahadundaruyar94@gmail.com>
2022-08-17 16:50:11 +0300

    usememo ornekleri yapıldı

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@ import React from "react";
 import Todos from './Todos';
 import {useState}from 'react';
 
+
+
+
+const cokZamanFonksiyon =(num)=>{
+  console.log("Hesaplanıyorr...");
+  for (let i = 0; i < 1000000; i++){
+    num += 1;
+  }
+  return num;
+}
+
+
+
+
 function App() {
   const [sayac, sayacGuncelle] =React.useState(0);
   const [todos, setTodos] = useState([]);
+  const hesaplamaSonucu = React.useMemo((sayac) =>cokZamanFonksiyon(sayac), []);
+  //hiçbir fonksiyona bağımlı değil anlamındadır boş array[]
+ 
 
   const increment = () => {
     sayacGuncelle((oncekiSayac) => oncekiSayac + 1);//!  tekrar render olacak
@@ -13,8 +30,12 @@ function App() {
     let saniye = new Date().getSeconds();
     setTodos((oncekiDizi) => [...oncekiDizi, "Yapılacak iş" + saniye]);//!!!
   }, []);
+
+
+
   return (
     <>
+    <p>Hesaplama Sonucu: {hesaplamaSonucu}</p>
       <Todos todos={todos} addTodo={addTodo} />
       <hr />
       <div>
