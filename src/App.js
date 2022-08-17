44882281d4b190fac919f02aee21b35aa9bb2fd9
SemahaDundar <semahadundaruyar94@gmail.com>
2022-08-17 16:13:19 +0300

    usecallback ornekleri todos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Todos from './Todos';
+import {useState}from 'react';
 
 function App() {
+  const [sayac, sayacGuncelle] =React.useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    sayacGuncelle((oncekiSayac) => oncekiSayac + 1);//!  tekrar render olacak
+  };
+  const addTodo = React.useCallback(() => {
+    let saniye = new Date().getSeconds();
+    setTodos((oncekiDizi) => [...oncekiDizi, "Yapılacak iş" + saniye]);//!!!
+  }, []);
   return (
- 
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Sayac: {sayac}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
   );
 }
 
